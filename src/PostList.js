5835ca521cc00d4e339d66e0369668a98875ad10
AdamPap <adampapamast@hotmail.com>
2021-05-03 21:30:00 +0300

    initial commit

---
+++ src/PostList.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+export default function PostList() {
+    const [posts, setPosts] = useState([])
+
+    const getPosts = async () => {
+        const posts = await axios({
+            method: 'GET',
+            url: 'http://localhost:3001/posts'
+        })
+        console.log(posts.data)
+        setPosts(posts.data)
+    }
+
+    return (
+        <div>
+            <button onClick={getPosts}>Get All Posts</button>
+            <ul>
+                {posts.map(post => {
+                    return <li>{post.title}</li>
+                })}
+            </ul>
+        </div>
+    )
+}
